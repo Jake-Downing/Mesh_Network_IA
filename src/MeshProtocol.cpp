bf6e3d4636db80623742423c6f3f90a4c4ba238d
Jake-Downing <jakesanuk@gmail.com>
2026-07-07 05:20:56 +0700

    Setup ESPNow

--- src/MeshProtocol.cpp
+++ src/MeshProtocol.cpp
@@ -10,6 +10,7 @@ uint8_t broadcastAddress[] = {
     0xFF,0xFF,0xFF
 };
 
+// OnDataSent(unmodifyable mac adress[ptr], status[enum: ESP_NOW_SEND_SUCCESS or else])
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 
     Serial.print("Send Status: ");
@@ -20,6 +21,7 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
         Serial.println("Failed");
 }
 
+// OnDataRecv(unmodifyable mac adress[ptr], unmodifyable incoming data[ptr], len)
 void OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len) {
 
     memcpy(&packet, incomingData, sizeof(packet));
@@ -38,8 +40,13 @@ void OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len) {
     Serial.println(packet.message);
 }
 
+// Setup for ESPNow broadcast
 void setupESPNow() {
 
+    if(esp_now_init() != ESP_OK) {
+        Serial.println("ESPNow Init Failed");
+        return;
+    }
 
 }
 
