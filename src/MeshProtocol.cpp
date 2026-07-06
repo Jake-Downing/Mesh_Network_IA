4a5758003edf3da336a1e583a2806373ca9b9c21
Jake-Downing <jakesanuk@gmail.com>
2026-07-07 05:07:37 +0700

    OnDataRecv function

--- src/MeshProtocol.cpp
+++ src/MeshProtocol.cpp
@@ -20,9 +20,22 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
         Serial.println("Failed");
 }
 
-void OnDataRecv() {
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len) {
 
+    memcpy(&packet, incomingData, sizeof(packet));
 
+    Serial.println("------------xxxx------------");
+    Serial.print("From: ");
+    Serial.println(packet.source);
+
+    Serial.print("To: ");
+    Serial.println(packet.destination);
+
+    Serial.print("Hops: ");
+    Serial.println(packet.hopCount);
+
+    Serial.print("Message: ");
+    Serial.println(packet.message);
 }
 
 void setupESPNow() {
